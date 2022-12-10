e678609e2e3e31e29d512341ca3f22edb2928116
dmitrii klochkov <d.klochkov9421@gmail.com>
2022-12-10 21:55:14 +0300

    tyr push Task19 Task21 Task23

---
+++ Task21/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Задача 21
+Напишите программу, которая принимает на вход координаты 
+    двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B(2, 1, -7), -> 15.84
+A(7, -5, 0); B(1, -1, 9)-> 11.53*/
+// Задаем переменные по осям X Y Z
+int A_x, A_y, A_z, B_x, B_y, B_z;
+
+// Переводим ввод строки в целые числа x y z
+Console.Write($"Введите координату x в точке A: ");
+
+// говорим что они целове.пробуем перевестию. читаем с линии. выводим в переменную 
+int.TryParse(Console.ReadLine(), out A_x);
+Console.Write($"Введите координату y в точке А: ");
+int.TryParse(Console.ReadLine(), out A_y);
+Console.Write($"Введите координату z в точке A: ");
+int.TryParse(Console.ReadLine(), out A_z);
+Console.Write($"Введите координату х в точке В: ");
+int.TryParse(Console.ReadLine(), out B_x);
+Console.Write($"Введите координату y в точке В: ");
+int.TryParse(Console.ReadLine(), out B_y);
+Console.Write($"Введите координату z в точке B: ");
+int.TryParse(Console.ReadLine(), out B_z);
+
+// вычитаем стороны B и А потом умножаем результат
+int x = (B_x - A_x) * (B_x - A_x);
+int y = (B_y - A_y) * (B_y - A_y);
+int z = (B_z - A_z) * (B_z - A_z);
+
+// складываем все рузультаты
+int xyz = x + y + z;
+
+// Round(Double) Округляет значение с плавающей запятой двойной точности до ближайшего целого значения; 
+//значения посередине округляются до ближайшего четного числа.
+double ans = Math.Round(Math.Sqrt(xyz), 2);
+
+//Math.Sqrt Возвращает квадратный корень из указанного числа.                                         
+Console.Write($"Расстояние между точками A и В = {ans}");
